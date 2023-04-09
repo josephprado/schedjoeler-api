306de3e307df99f0c0d50a8629c70ccc9a19c711
prado <pradodje@gmail.com>
2023-04-09 14:25:03 -0700

    Implement user controller methods

--- src/main/java/com/portfolijo/schedjoeler/controller/UserController.java
+++ src/main/java/com/portfolijo/schedjoeler/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.portfolijo.schedjoeler.controller;
 
+import com.portfolijo.schedjoeler.converter.UserConverter;
 import com.portfolijo.schedjoeler.domain.User;
 import com.portfolijo.schedjoeler.dto.UserCreateDto;
 import com.portfolijo.schedjoeler.dto.UserDto;
@@ -22,37 +23,76 @@ import java.util.UUID;
 @RequestMapping("/api/users")
 public class UserController extends Controller<UserDtoBase> {
     private final UserService SVC;
+    private final UserConverter CON;
 
+    /**
+     * Gets the identified user
+     *
+     * @param uuid A user uuid
+     * @return A response entity containing a user
+     */
     @GetMapping("/{uuid}")
     public ResponseEntity<Response<UserDto>> getOne(@PathVariable(name = "uuid") UUID uuid) {
-        UserDto data = convert(SVC.findOne(uuid));
+        User user = SVC.findOne(uuid);
+        UserDto data = CON.toDto(user);
         return responseCodeOk(List.of(data));
     }
 
+    /**
+     * Gets all users
+     *
+     * @return A response entity containing a list of users
+     */
     @GetMapping("")
     public ResponseEntity<Response<UserDto>> getAll() {
-        List<UserDto> data = SVC.findAll().stream().map(this::convert).toList();
+        List<UserDto> data = SVC
+                .findAll()
+                .stream()
+                .map(CON::toDto)
+                .toList();
         return responseCodeOk(data);
     }
 
+    /**
+     * Saves the given user
+     *
+     * @param dto A user create DTO
+     * @return A response entity containing the saved user
+     */
     @PostMapping("")
     public ResponseEntity<Response<UserDto>> saveOne(@Valid @RequestBody UserCreateDto dto) {
-        return null;
+        User user = CON.toUser(dto);
+        user = SVC.saveOne(user);
+        UserDto data = CON.toDto(user);
+        return responseCodeCreated(List.of(data), "/"+data.getUuid());
     }
 
+    /**
+     * Updates the identified user
+     *
+     * @param uuid A user uuid
+     * @param dto A user update DTO
+     * @return A response entity containing the updated user
+     */
     @PatchMapping("/{uuid}")
     public ResponseEntity<Response<UserDto>> updateOne(@PathVariable(name = "uuid") UUID uuid,
                                                        @Valid @RequestBody UserUpdateDto dto) {
-        return null;
+        User user = CON.toUser(uuid, dto);
+        user = SVC.saveOne(user);
+        UserDto data = CON.toDto(user);
+        return responseCodeOk(List.of(data));
     }
 
+    /**
+     * Deletes the identified user
+     *
+     * @param uuid A user uuid
+     * @return A response entity containing the result of the deletion
+     * @throws Exception If the deletion was unsuccessful
+     */
     @DeleteMapping("/{uuid}")
     public ResponseEntity<Response<UserDto>> deleteOne(@PathVariable(name = "uuid") UUID uuid) throws Exception {
         SVC.deleteOne(uuid);
         return responseCodeNoContent();
-    }
-
-    public UserDto convert(User user) {
-        return UserDto.builder().build();
     }
 }
